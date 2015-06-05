7658d98ddb8b407eaf8de0911db7a409f4f73d58
Brandon Long <blong1996@gmail.com>
2015-06-04 23:24:06 -0400

    edits

--- ParkApp/www/js/controllers.js
+++ ParkApp/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [ ])
 
-.controller("CameraCtrl", function($scope, $cordovaCamera, Camera) {
+.controller("CameraCtrll", function($scope, $cordovaCamera, Camera) {
  
     $scope.takePicture = function() {
         var options = { 
@@ -24,6 +24,40 @@ angular.module('starter.controllers', [ ])
     }
  
 })
+
+
+.controller('CameraCtrl', function ($scope) {        
+     $scope.takePic = function() {
+        var options =   {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+            encodingType: 0     // 0=JPG 1=PNG
+        }
+        navigator.camera.getPicture(onSuccess,onFail,options);
+    }
+    var onSuccess = function(FILE_URI) {
+        console.log(FILE_URI);
+        $scope.picData = FILE_URI;
+        $scope.$apply();
+    };
+    var onFail = function(e) {
+        console.log("On fail " + e);
+    }
+    $scope.send = function() {   
+        var myImg = $scope.picData;
+        var options = new FileUploadOptions();
+        options.fileKey="post";
+        options.chunkedMode = false;
+        var params = {};
+        params.user_token = localStorage.getItem('auth_token');
+        params.user_email = localStorage.getItem('email');
+        options.params = params;
+        var ft = new FileTransfer();
+        ft.upload(myImg, encodeURI("https://example.com/posts/"), onUploadSuccess, onUploadFail, options);
+    }
+  })
+
 
 .controller('DashCtrl', function($scope) {})
 
@@ -64,16 +98,10 @@ angular.module('starter.controllers', [ ])
   }, cameraOptions);
 })*/
 
-.controller('CameraCtrl', function($scope, Camera) {
+//.controller('CameraCtrl', function($scope, Camera) {
 
-  $scope.getPhoto = function() {
-    Camera.getPicture().then(function(imageURI) {
-      console.log(imageURI);
-    }, function(err) {
-      console.err(err);
-    });
-  };
-})
+
+
 
 .controller('ProfileCtrl', function($scope) {})
 
