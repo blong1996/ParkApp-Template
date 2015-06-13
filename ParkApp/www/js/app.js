4b69568c55921a7f7328b3a951e6c6ecff23fc6b
Brandon Long <blong1996@gmail.com>
2015-06-13 19:35:38 -0400

    fixed option buttons, cleaned some code

--- ParkApp/www/js/app.js
+++ ParkApp/www/js/app.js
@@ -24,16 +24,13 @@ angular.module('starter', ['ionic','ngCordova',  'starter.controllers', 'starter
 	// Set up the various states which the app can be in.
 	// Each state's controller can be found in controllers.js
 	$stateProvider
-
 	// setup an abstract state for the tabs directive
 		.state('tab', {
 		url: "/tab",
 		abstract: true,
 		templateUrl: "templates/tabs.html"
 	})
-
 	// Each tab has its own nav history stack:
-
 		.state('tab.profile-options', {
 			url: '/profile/options',
 			views: {
@@ -43,23 +40,75 @@ angular.module('starter', ['ionic','ngCordova',  'starter.controllers', 'starter
 				}
 			}
 		})
-
 		.state('tab.options-edit', {
-			url: '/options/edit',
+			url: '/profile/options/edit',
 			views: {
-				'profile-options': {
+				'tab-profile': {
 					templateUrl: 'templates/options-edit.html',
 					controller: 'OptionsEditCtrl'
 				}
 			}
 		})
-
 		.state('tab.options-liked', {
-			url: '/options/liked',
+			url: '/profile/options/liked',
 			views: {
-				'profile-options': {
+				'tab-profile': {
 					templateUrl: 'templates/options-liked.html',
 					controller: 'OptionsLikedCtrl'
+				}
+			}
+		})
+		.state('tab.options-logout', {
+			url: '/profile/options/logout',
+			views: {
+				'tab-profile': {
+					templateUrl: 'templates/options-logout.html',
+					controller: 'OptionsLogoutCtrl'
+				}
+			}
+		})
+		.state('tab.options-privacy', {
+			url: '/profile/options/privacy',
+			views: {
+				'tab-profile': {
+					templateUrl: 'templates/options-privacy.html',
+					controller: 'OptionsPrivacyCtrl'
+				}
+			}
+		})
+		.state('tab.options-problems', {
+			url: '/profile/options/problems',
+			views: {
+				'tab-profile': {
+					templateUrl: 'templates/options-problems.html',
+					controller: 'OptionsProblemsCtrl'
+				}
+			}
+		})
+		.state('tab.options-reset', {
+			url: '/profile/options/reset',
+			views: {
+				'tab-profile': {
+					templateUrl: 'templates/options-reset.html',
+					controller: 'OptionsResetCtrl'
+				}
+			}
+		})
+		.state('tab.options-terms', {
+			url: '/profile/options/terms',
+			views: {
+				'tab-profile': {
+					templateUrl: 'templates/options-terms.html',
+					controller: 'OptionsTermsCtrl'
+				}
+			}
+		})
+		.state('tab.options-delete', {
+			url: '/profile/options/delete',
+			views: {
+				'tab-profile': {
+					templateUrl: 'templates/options-delete.html',
+					controller: 'OptionsDeleteCtrl'
 				}
 			}
 		})
@@ -72,7 +121,6 @@ angular.module('starter', ['ionic','ngCordova',  'starter.controllers', 'starter
 				}
 			}
 		}) 
-
 		.state('tab.notifications', {
 			url: '/notifications',
 			views: {
@@ -82,7 +130,6 @@ angular.module('starter', ['ionic','ngCordova',  'starter.controllers', 'starter
 				}
 			}
 		}) 
-
 		.state('tab.profile', {
 			url: '/profile',
 			views: {
@@ -92,7 +139,6 @@ angular.module('starter', ['ionic','ngCordova',  'starter.controllers', 'starter
 				}
 			}
 		}) 
-
 		.state('tab.map', {
 			url: '/map',
 			views: {
@@ -112,212 +158,10 @@ angular.module('starter', ['ionic','ngCordova',  'starter.controllers', 'starter
 			}
 		});
 
-
-
 	// if none of the above states are matched, use this as the fallback
 	$urlRouterProvider.otherwise('/tab/home');
 
 });
 
-/*
- * SimplePubSub from https://github.com/mbenford/ngTagsInput/blob/master/src/util.js
- * */
-'use strict';
-var app = angular.module('slidebox', ['ionic', 'style'])
-		.run(['$q', '$http', '$rootScope', '$location', '$window', '$timeout', 
-					function($q, $http, $rootScope, $location, $window, $timeout){
-			
-					$rootScope.$on("$locationChangeStart", function(event, next, current){
-							$rootScope.error = null;
-							console.log("Route change!!!", $location.path());
-							var path = $location.path();
-							
-							
-							console.log("App Loaded!!!");
-					});
-			}
-			]);
-		
-		app.config(function($stateProvider, $urlRouterProvider) {
-			$stateProvider.state('index', {
-				url : '/',
-				templateUrl : 'index.html',
-				controller : 'IndexCtrl'
-			});
-		
-			$urlRouterProvider.otherwise("/");
-		});
-		
-				app.controller("IndexCtrl", ['$rootScope', "$scope", "$stateParams", "$q", "$location", "$window", '$timeout', 
-			function($rootScope, $scope, $stateParams, $q, $location, $window, $timeout){
-			$scope.onSlideMove = function(data){
-				alert("You have selected " + data.index + " tab");
-			};  
-				}
-				]);
 
-function SimplePubSub() {
-		var events = {};
-		return {
-				on: function(names, handler) {
-						names.split(' ').forEach(function(name) {
-								if (!events[name]) {
-										events[name] = [];
-								}
-								events[name].push(handler);
-						});
-						return this;
-				},
-				trigger: function(name, args) {
-						angular.forEach(events[name], function(handler) {
-								handler.call(null, args);
-						});
-						return this;
-				}
-		};
-};
-
-angular.module('tabSlideBox', [])
-.directive('onFinishRender', function ($timeout) {
-		return {
-				restrict: 'A',
-				link: function (scope, element, attr) {
-						if (scope.$last === true) {
-								$timeout(function () {
-										scope.$emit('ngRepeatFinished');
-								});
-						}
-				}
-		}
-})
-.directive('tabSlideBox', [ '$timeout', '$window', '$ionicSlideBoxDelegate', '$ionicScrollDelegate',
-	function($timeout, $window, $ionicSlideBoxDelegate, $ionicScrollDelegate) {
-		'use strict';
-
-		return {
-			restrict : 'A, E, C',
-			link : function(scope, element, attrs, ngModel) {
-				
-				var ta = element[0], $ta = element;
-				$ta.addClass("tabbed-slidebox");
-				if(attrs.tabsPosition === "bottom"){
-					$ta.addClass("btm");
-				}
-				
-				//Handle multiple slide/scroll boxes
-				var handle = ta.querySelector('.slider').getAttribute('delegate-handle');
-				
-				var ionicSlideBoxDelegate = $ionicSlideBoxDelegate;
-				if(handle){
-					ionicSlideBoxDelegate = ionicSlideBoxDelegate.$getByHandle(handle);
-				}
-				
-				var ionicScrollDelegate = $ionicScrollDelegate;
-				if(handle){
-					ionicScrollDelegate = ionicScrollDelegate.$getByHandle(handle);
-				}
-				
-				function renderScrollableTabs(){
-					var iconsDiv = angular.element(ta.querySelector(".tsb-icons")), icons = iconsDiv.find("a"), wrap = iconsDiv[0].querySelector(".tsb-ic-wrp"), totalTabs = icons.length;
-					var scrollDiv = wrap.querySelector(".scroll");
-					
-					angular.forEach(icons, function(value, key){
-							 var a = angular.element(value);
-							 a.on('click', function(){
-								 ionicSlideBoxDelegate.slide(key);
-							 });
-
-						if(a.attr('icon-off')) {
-							a.attr("class", a.attr('icon-off'));
-						}
-					});
-					
-					var initialIndex = attrs.tab;
-					//Initializing the middle tab
-					if(typeof attrs.tab === 'undefined' || (totalTabs <= initialIndex) || initialIndex < 0){
-						initialIndex = Math.floor(icons.length/2);
-					}
-					
-					//If initial element is 0, set position of the tab to 0th tab 
-					if(initialIndex == 0){
-						setPosition(0);
-					}
-					
-					$timeout(function() {
-						ionicSlideBoxDelegate.slide(initialIndex);
-					}, 0);
-				}
-				function setPosition(index){
-					var iconsDiv = angular.element(ta.querySelector(".tsb-icons")), icons = iconsDiv.find("a"), wrap = iconsDiv[0].querySelector(".tsb-ic-wrp"), totalTabs = icons.length;
-					var scrollDiv = wrap.querySelector(".scroll");
-					
-					var middle = iconsDiv[0].offsetWidth/2;
-					var curEl = angular.element(icons[index]);
-					var prvEl = angular.element(iconsDiv[0].querySelector(".active"));
-					if(curEl && curEl.length){
-					var curElWidth = curEl[0].offsetWidth, curElLeft = curEl[0].offsetLeft;
-
-					if(prvEl.attr('icon-off')) {
-						prvEl.attr("class", prvEl.attr('icon-off'));
-					}else{
-						prvEl.removeClass("active");
-					}
-					if(curEl.attr('icon-on')) {
-						curEl.attr("class", curEl.attr('icon-on'));
-					}
-					curEl.addClass("active");
-					
-					var leftStr = (middle  - (curElLeft) -  curElWidth/2 + 5);
-					//If tabs are not scrollable
-					if(!scrollDiv){
-						var leftStr = (middle  - (curElLeft) -  curElWidth/2 + 5) + "px";
-						wrap.style.webkitTransform =  "translate3d("+leftStr+",0,0)" ;
-					}else{
-						//If scrollable tabs
-						var wrapWidth = wrap.offsetWidth;
-						var currentX = Math.abs(getX(scrollDiv.style.webkitTransform));
-						var leftOffset = 100;
-						var elementOffset = 40;
-						//If tabs are reaching right end or left end
-						if(((currentX + wrapWidth) < (curElLeft + curElWidth + elementOffset)) || (currentX > (curElLeft - leftOffset))){
-							if(leftStr > 0){
-								leftStr = 0;
-							}
-							//Use this scrollTo, so when scrolling tab manually will not flicker
-							ionicScrollDelegate.scrollTo(Math.abs(leftStr), 0, true);
-						}
-					}
-					}
-				};
-				function getX(matrix) {
-					matrix = matrix.replace("translate3d(","");
-					matrix = matrix.replace("translate(","");
-					return (parseInt(matrix));
-				}
-				var events = scope.events;
-				events.on('slideChange', function(data){
-					setPosition(data.index);
-				});
-				events.on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-					renderScrollableTabs();
-				});
-				
-				renderScrollableTabs();
-			},
-			controller : function($scope, $attrs, $element) {
-				$scope.events = new SimplePubSub();
-				
-				$scope.slideHasChanged = function(index){
-					$scope.events.trigger("slideChange", {"index" : index});
-					$timeout(function(){if($scope.onSlideMove) $scope.onSlideMove({"index" : eval(index)});},100);
-				};
-				
-				$scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-					$scope.events.trigger("ngRepeatFinished", {"event" : ngRepeatFinishedEvent});
-				});
-			}
-		};
-
-	} 
-]);
 
--- ParkApp/www/js/controllers.js
+++ ParkApp/www/js/controllers.js
@@ -1,112 +1,106 @@
 angular.module('starter.controllers', [ ])
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+	 
 .controller('ProfileOptionsCtrl', function() {})
+	// profile-options controller
 .controller('OptionsEditCtrl', function() {})
+	// options-edit controller
 .controller('OptionsLikedCtrl', function() {})
+	// options-liked controller
+.controller('OptionsLogoutCtrl', function() {})
+	// options-logout controller
+.controller('OptionsPrivacyCtrl', function() {})
+	// options-privacy controller
+.controller('OptionsDeleteCtrl', function() {})
+	// options-delete controller
+.controller('OptionsResetCtrl', function() {})
+	// options-reset controller
+.controller('OptionsTermsCtrl', function() {})
+	// options-terms controller
+.controller('OptionsProblemsCtrl', function() {})
+	// options-problems controller
 .controller('HomeCtrl', function($scope) {})
-
+	// home controller
 .controller('NotificationsCtrl', function($scope) {})
-
-
+	// notifications controller
 .controller('CameraCtrl', function($scope, Camera) {
-
-   
-    var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value
-
-    // Wait for device API libraries to load
-    //
-    document.addEventListener("deviceready",onDeviceReady,false);
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
-
-    // Called when a photo is successfully retrieved
-    //
-     function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64-encoded image data
-      // console.log(imageData);
-
-      // Get image handle
-      //
-      var smallImage = document.getElementById('smallImage');
-
-      // Unhide image elements
-      //
-      smallImage.style.display = 'block';
-
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
-
-    // Called when a photo is successfully retrieved
-    //
-    $scope.onPhotoURISuccess = function(imageURI) {
-      // Uncomment to view the image file URI
-      // console.log(imageURI);
-
-      // Get image handle
-      //
-      var largeImage = document.getElementById('largeImage');
-
-      // Unhide image elements
-      //
-      largeImage.style.display = 'block';
-
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      largeImage.src = imageURI;
-    }
-
-    // A button will call this function
-    //
-    $scope.capturePhoto = function() {
-      // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    // A button will call this function
-    //
-    $scope.capturePhotoEdit = function() {
-      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-        destinationType: destinationType.DATA_URL });
-    }
-
-    // A button will call this function
-    //
-    $scope.getPhoto = function(source) {
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
-
-    // Called if something bad happens.
-    //
-    $scope.onFail = function(message) {
-      alert('Failed because: ' + message);
-    }
-
-
+	// cmera controller
+		var pictureSource;   // picture source
+		var destinationType; // sets the format of returned value
+		// Wait for device API libraries to load
+		//
+		document.addEventListener("deviceready",onDeviceReady,false);
+		// device APIs are available
+		//
+		function onDeviceReady() {
+				pictureSource=navigator.camera.PictureSourceType;
+				destinationType=navigator.camera.DestinationType;
+		}
+		// Called when a photo is successfully retrieved
+		//
+		 function onPhotoDataSuccess(imageData) {
+			// Uncomment to view the base64-encoded image data
+			// console.log(imageData);
+			// Get image handle
+			//
+			var smallImage = document.getElementById('smallImage');
+			// Unhide image elements
+			//
+			smallImage.style.display = 'block';
+			// Show the captured photo
+			// The inline CSS rules are used to resize the image
+			//
+			smallImage.src = "data:image/jpeg;base64," + imageData;
+		}
+		// Called when a photo is successfully retrieved
+		$scope.onPhotoURISuccess = function(imageURI) {
+			// Uncomment to view the image file URI
+			// console.log(imageURI);
+			// Get image handle
+			var largeImage = document.getElementById('largeImage');
+			// Unhide image elements
+			//
+			largeImage.style.display = 'block';
+			// Show the captured photo
+			// The inline CSS rules are used to resize the image
+			//
+			largeImage.src = imageURI;
+		}
+		// A button will call this function
+		//
+		$scope.capturePhoto = function() {
+			// Take picture using device camera and retrieve image as base64-encoded string
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+				destinationType: destinationType.DATA_URL });
+		}
+		// A button will call this function
+		//
+		$scope.capturePhotoEdit = function() {
+			// Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+				destinationType: destinationType.DATA_URL });
+		}
+		// A button will call this function
+		//
+		$scope.getPhoto = function(source) {
+			// Retrieve image file location from specified source
+			navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+				destinationType: destinationType.FILE_URI,
+				sourceType: source });
+		}
+		// Called if something bad happens.
+		//
+		$scope.onFail = function(message) {
+			alert('Failed because: ' + message);
+		}
 })
-
 .controller("ExampleController", function($scope, $cordovaCamera, Camera) {
  var pictureSource;   // picture source
     var destinationType; // sets the format of returned value
@@ -215,9 +209,9 @@ angular.module('starter.controllers', [ ])
  
 })
 
+
 .controller('ProfileCtrl', function($scope) {})
-
-
+	// profile controller
 .controller('MapCtrl', function($scope) {});
-
+	// map controller
 
